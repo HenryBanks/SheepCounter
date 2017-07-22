e01ba2429a81eeb7755f7854d3f84a526f363eb9
Anjola Olubusi <anjolaolubusi@gmail.com>
2017-07-22 23:51:02 +0400

    Added Music and a Point Sound

---
+++ Scripts/sheepCount.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sheepCount : MonoBehaviour {
+
+    public AudioClip coinClip;
+    private AudioSource source;
+    public float lowPitchRange; //.75F
+    public float highPitchRange; // 1.5F
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown(){
+		scoreManager.instance.addToScore (10);
+        source.pitch = Random.Range(lowPitchRange, highPitchRange);
+        source.PlayOneShot(coinClip);
+		Destroy (this);
+	}
+
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+}
---
+++ Scripts/sheepJump.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sheepJump : MonoBehaviour {
+
+    // How far the sheep will move
+    public float upmove;
+    
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform) {
+            print(other.transform.tag);
+            if (other.transform.tag == "sheep") {
+                other.transform.Translate(new Vector3(0, upmove, 0));
+                
+            }
+        }
+    }
+}
