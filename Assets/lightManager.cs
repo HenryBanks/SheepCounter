1ce596ca76d1b6baf350e39203ca766fe84597f9
Henry Banks <harry_b_15@hotmail.co.uk>
2017-08-01 20:56:41 +0100

    colour changes at multiplier > 5

---
+++ Assets/lightManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightManager : MonoBehaviour {
+
+	private Light thisLight;
+
+	private float duration=1f;
+
+	// Use this for initialization
+	void Start () {
+		thisLight = GetComponent<Light> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (scoreManager.multiplier>5) {
+			float t = Mathf.PingPong (Time.time, duration) / duration;
+			thisLight.color = Color.Lerp (Color.white, Color.red, t);
+		} else {
+			thisLight.color = Color.white;
+		}
+	}
+}
